ea4ed1b6e7a3d08285f87c9addec0367da0259a0
changwann <changwan@gm.gist.ac.kr>
2023-07-04 17:12:05 +0900

    week1 7.4 ver.

--- taplayout2/app/src/main/java/com/example/taplayout2/MainActivity.java
+++ taplayout2/app/src/main/java/com/example/taplayout2/MainActivity.java
@@ -14,12 +14,13 @@ import android.os.Bundle;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    // 클래스 선언
+    private PermissionSupport permission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         ViewPager pager = findViewById(R.id.pager);
         TabLayout tabLayout = findViewById(R.id.tab_layout);
@@ -29,8 +30,32 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(pager); //텝레이아웃과 뷰페이저를 연결
         pager.setAdapter(new PageAdapter(getSupportFragmentManager(),this)); //뷰페이저 어뎁터 설정 연결
 
-
+        permissionCheck();
     }
+
+    private void permissionCheck() {
+
+        // PermissionSupport.java 클래스 객체 생성
+        permission = new PermissionSupport(this, this);
+
+        // 권한 체크 후 리턴이 false로 들어오면
+        if (!permission.checkPermission()){
+            //권한 요청
+            permission.requestPermission();
+        }
+    }
+
+    // Request Permission에 대한 결과 값 받아와
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //여기서도 리턴이 false로 들어온다면 (사용자가 권한 허용 거부)
+        if (!permission.permissionResult(requestCode, permissions, grantResults)) {
+            // 다시 permission 요청
+            permission.requestPermission();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
     static class PageAdapter extends FragmentStatePagerAdapter { //뷰 페이저 어뎁터
 
 
--- taplayout2/app/src/main/java/com/example/taplayout2/TabFragment1.java
+++ taplayout2/app/src/main/java/com/example/taplayout2/TabFragment1.java
@@ -1,17 +1,28 @@
 package com.example.taplayout2;
 
+import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.Vibrator;
 import android.provider.ContactsContract;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextWatcher;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -20,6 +31,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TabFragment1 extends Fragment {
@@ -58,6 +72,68 @@ public class TabFragment1 extends Fragment {
         }
     }
 
+    public void ContactAdd(String name, String number){
+        new Thread(){
+            @Override
+            public void run() {
+
+                ArrayList<ContentProviderOperation> list = new ArrayList<>();
+                try{
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                                    .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                                    .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                                    .build()
+                    );
+
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)   //이름
+
+                                    .build()
+                    );
+
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, number)           //전화번호
+                                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE  , ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)   //번호타입(Type_Mobile : 모바일)
+
+                                    .build()
+                    );
+                    /*
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.Email.DATA  , "hong_gildong@naver.com")  //이메일
+                                    .withValue(ContactsContract.CommonDataKinds.Email.TYPE  , ContactsContract.CommonDataKinds.Email.TYPE_WORK)     //이메일타입(Type_Work : 직장)
+
+                                    .build()
+                    );
+                    */
+                    getActivity().getApplicationContext().getContentResolver().applyBatch(ContactsContract.AUTHORITY, list);  //주소록추가
+                    list.clear();   //리스트 초기화
+                }catch(RemoteException e){
+                    e.printStackTrace();
+                }catch(OperationApplicationException e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
+    public void mOnContactAdd(View v, String name, String number) {
+        if(v.getId()!=R.id.btnContactAdd) { return; }
+        ContactAdd(name, number);
+    }
 
     @Nullable
     @Override
@@ -88,12 +164,81 @@ public class TabFragment1 extends Fragment {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity().getApplicationContext(), "해당 연락처 삭제 중", Toast.LENGTH_SHORT).show();
                 String contactName = contacts.get(position).first;
                 deleteContact(contactName);
-                displayContacts.remove(position); // 얘가 무슨 역할을 하지? 바로 새로고침은 안되고 다른 탭 들어갔다 오면 작동하네.
+                displayContacts.remove(position);
+                contacts.remove(position);
+                listView.setAdapter(listViewAdapter);
                 Toast.makeText(getActivity().getApplicationContext(), "해당 연락처 삭제 완료", Toast.LENGTH_SHORT).show();
                 return true;
+            }
+        });
+
+        View button = view.findViewById(R.id.btnContactAdd);
+        EditText name_edittext = (EditText)view.findViewById(R.id.editText1);
+        EditText number_edittext = (EditText)view.findViewById(R.id.editText2);
+        // 숫자와 "-"만 입력 가능하게 하는 InputFilter
+        InputFilter filter = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                for (int i = start; i < end; i++) {
+                    if (!Character.isDigit(source.charAt(i)) && source.charAt(i) != '-') {
+                        return "";
+                    }
+                }
+                return null;
+            }
+        };
+        number_edittext.setFilters(new InputFilter[]{filter});
+
+        // 전화번호 형식에 맞게 자동으로 "-" 추가
+        number_edittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().startsWith("010")) {
+                    number_edittext.setText("010");
+                    number_edittext.setSelection(number_edittext.getText().length());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String input = s.toString();
+                String phoneNumber = input.replaceAll("-", "");
+                if (phoneNumber.length() > 8) {
+                    phoneNumber = phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3, 7) + "-" + phoneNumber.substring(7);
+                } else if (phoneNumber.length() > 3) {
+                    phoneNumber = phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3);
+                }
+
+                if (!input.equals(phoneNumber)) {
+                    number_edittext.setText(phoneNumber);
+                    number_edittext.setSelection(phoneNumber.length());
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContactAdd(name_edittext.getText().toString(), number_edittext.getText().toString());
+                Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(100);
+                displayContacts.add(name_edittext.getText().toString() + ": " + number_edittext.getText().toString());
+                Collections.sort(displayContacts);
+                contacts.add(new Pair<>(name_edittext.getText().toString(), number_edittext.getText().toString()));
+                Collections.sort(contacts, new Comparator<Pair<String, String>>() {
+                    @Override
+                    public int compare(Pair<String, String> o1, Pair<String, String> o2) {
+                        return o1.first.compareTo(o2.first);
+                    }
+                });
+                listView.setAdapter(listViewAdapter);
+                Toast.makeText(getActivity().getApplicationContext(), "연락처 추가 완료", Toast.LENGTH_SHORT).show();
             }
         });
 
